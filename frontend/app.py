f6c3afbbef5e7d7e47618498e7eaebddaf155a7e
Dylan Theriot <dylantheriot@gmail.com>
2020-02-15 19:07:42 -0600

    updated html files

--- frontend/app.py
+++ frontend/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 app = Flask(__name__)
-app.debug = True
+app.config['ENV'] = 'development'
+app.config['DEBUG'] = True
+app.config['TESTING'] = True
 
 @app.route('/')
 def hello_world():
@@ -9,3 +11,7 @@ def hello_world():
 @app.route('/create')
 def create():
     return render_template('create.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
