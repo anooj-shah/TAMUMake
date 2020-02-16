71d2517a30f89bf5d5b16379e83174001ef74127
Anooj Shah <thisisanooj@gmail.com>
2020-02-15 22:32:16 -0600

    serial/contact form/computer vision

--- frontend/app.py
+++ frontend/app.py
@@ -1,10 +1,12 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 import matplotlib.pyplot as plt
 import json
 from PIL import Image
 from io import BytesIO
+import serial
+
 
 
 app = Flask(__name__)
@@ -28,11 +30,24 @@ def about():
 def temp():
     return render_template('work-single.html')
 
+@app.route('/temp2')
+def temp3():
+    return render_template('contact.html')
+
 @app.route('/temp2', methods=['POST'])
 def temp2():
-    name = request.form['name']
-    image_url = request.form['image_url']
-    id = request.args.get('id')
+    # ser = serial.Serial('/dev/ttyUSB0')
+    # print(ser.name)
+
+    yourName = request.form['yourName']
+    recipientName = request.form['recipientName']
+    address = request.form['address']
+    image_url = request.form['imageURL']
+    message = request.form['message']
+
+    print(yourName, message)
+    # id = request.args.get('id')
+
 
     if 'COMPUTER_VISION_SUBSCRIPTION_KEY' in os.environ:
         subscription_key = os.environ['COMPUTER_VISION_SUBSCRIPTION_KEY']
@@ -46,7 +61,7 @@ def temp2():
     analyze_url = endpoint + "vision/v2.1/analyze"
 
     # image_url = "https://image.shutterstock.com/image-photo/close-photo-cheerful-excited-glad-260nw-789414166.jpg"
-
+    # USE THIS::: https://images-na.ssl-images-amazon.com/images/I/61wtSL1Yd6L._AC_SX425_.jpg
     headers = {'Ocp-Apim-Subscription-Key': subscription_key}
     params = {'visualFeatures': 'Categories,Description,Color'}
     data = {'url': image_url}
@@ -58,4 +73,9 @@ def temp2():
     image_caption = analysis["description"]["captions"][0]["text"].capitalize()
 
     print(image_caption)
-    return image_caption
+    # messageToSend = recipientName + '-' + message + '-' + image_caption + '-' + yourName
+    # messageToSend = messageToSend.encode('utf-8')
+    # ser.write(messageToSend)
+
+    ser.close()
+    return render_template('contact.html')
